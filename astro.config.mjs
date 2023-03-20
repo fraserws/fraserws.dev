0df15ad9318cc9555da14ce02478322536b40cc5
fraserws <fraserws.uk2@gmail.com>
2023-03-20 12:57:30 +0000

    major changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,34 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import { defineConfig } from "astro/config";
 
-import sitemap from '@astrojs/sitemap';
+// https://astro.build/config
+
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
+
+// https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap({
+    serialize(item) {
+      if (/exclude-from-sitemap/.test(item.url)) {
+        return undefined;
+      }
+      if (/your-special-page/.test(item.url)) {
+        item.changefreq = "daily";
+        item.lastmod = new Date();
+        item.priority = 0.9;
+      }
+      return item;
+    }
+  }), tailwind(), prefetch(), preact()]
 });
---
+++ src/pages/blog/test.md
@@ -0,0 +1,8 @@
+---
+layout: /src/layouts/Post.astro
+title: First post
+summary: This is my first post
+publishedAt: "2023-03-20"
+---
+
+# Hello world
