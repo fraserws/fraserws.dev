db29ed2b72dd494004c168bb13753537c1692952
Fraser <fraserws.uk2@gmail.com>
2023-09-08 00:17:02 +0100

    scroll-bar fixes

--- src/components/ScrollProgressBar.tsx
+++ src/components/ScrollProgressBar.tsx
@@ -19,10 +19,10 @@ export default function ProgressBar() {
     };
   }, []);
 
-  const gradientColors = "bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500"; // Customize your gradient colors
+  const gradientColors = "bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 ";
 
   return (
-    <div class="fixed top-0 left-0 w-full h-1 z-50  rounded-t opacity-75">
+    <div class="fixed top-0 left-0 w-full h-2 z-50 overflow-hidden">
       <div
         class={`h-full ${gradientColors}`}
         style={{ width: `${scrollPercentage}%` }}
