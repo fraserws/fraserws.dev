6d9a79ab5c981a27c85be2751fa7fae374eee783
Fraser <fraserws.uk2@gmail.com>
2024-01-16 23:11:58 +0000

    static vercel build

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import prefetch from "@astrojs/prefetch";
 import preact from "@astrojs/preact";
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/static';
 export default defineConfig({
   site: "https://fraserws.dev",
-  output: 'server',
+  output: 'static',
   adapter: vercel({
     webAnalytics: {
       enabled: true,
